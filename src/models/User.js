27c2aba31456193e59858232910b45adaba7f28b
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-04-06 08:53:11 -0300

    Added validation for User model and routes api/signup - api/signin

--- src/models/User.js
+++ src/models/User.js
@@ -16,15 +16,41 @@ module.exports = (sequelize, DataTypes) => {
   User.init({
     name: {
       allowNull: false,
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      validate: {
+        isAlphanumeric: {
+          args: true,
+          msg: 'Name must be alphabetical'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Name is required'
+        },
+        len: {
+          args: [2, 30],
+          msg: 'Name must be between 2 and 30 characters'
+        }
+      }
     },
     password: {
       allowNull: false,
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [6, 255],
+          msg: 'Password must be between 6 and 30 characters'
+        }
+      }
     },
     email: {
       allowNull: false,
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Email must be a valid email'
+        }
+      }
     }
   }, {
     sequelize,
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
+const { signIn, signUp } = require('../controllers/auth.crtl');
 
 router.get('/', (req, res) => {
     res.json({
         msg: 'Hello World'
     });
 });
+
+router.post('/signin', signIn);
+router.post('/signup', signUp);
 
 module.exports = router;
