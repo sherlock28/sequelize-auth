fc4231437629a72d87d80cbd2280c1631b1b53a1
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-04-07 08:28:51 -0300

    Added signin function to auth controller and fix response for signup

--- src/controllers/auth.crtl.js
+++ src/controllers/auth.crtl.js
@@ -5,7 +5,66 @@ const authConfig = require('../../config/auth');
 
 module.exports = {
     signIn(req, res) {
+        let { email, password } = req.body;
 
+        User.findOne({
+            where: {
+                email
+            }
+        })
+            .then(user => {
+                if (!user) {
+                    return res.status(400).send({
+                        success: false,
+                        payload: null,
+                        msg: 'Invalid email or password',
+                        error: null
+                    });
+                }
+
+                if (!bcrypt.compareSync(password, user.password)) {
+                    return res.status(400).send({
+                        success: false,
+                        payload: null,
+                        msg: 'Invalid email or password',
+                        error: null
+                    });
+                }
+
+                const userDto = {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                    createdAt: user.createdAt,
+                    updatedAt: user.updatedAt
+                }
+
+                const token = jwt.sign({ user: userDto }, authConfig.secret, {
+                    expiresIn: authConfig.expiresIn
+                });
+
+                return res.send({
+                    success: true,
+                    payload: {
+                        user,
+                        token
+                    },
+                    msg: 'User logged in successfully',
+                    error: null
+                });
+            })
+            .catch(err => {
+                return res.status(400).send({
+                    success: false,
+                    payload: null,
+                    msg: 'Error on the server.',
+                    error: {
+                        code: 500,
+                        msg: 'Internal server error',
+                        err: err,
+                    }
+                });
+            });
     },
     signUp(req, res) {
         let password = bcrypt.hashSync(req.body.password, +authConfig.rounds);
@@ -15,13 +74,22 @@ module.exports = {
             email: req.body.email,
             password
         }).then(user => {
-            let token = jwt.sign({ id: user.id }, authConfig.secret, {
+            const userDto = {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: user.createdAt,
+                updatedAt: user.updatedAt
+            }
+
+            let token = jwt.sign({ user: userDto }, authConfig.secret, {
                 expiresIn: authConfig.expiresIn
             });
+
             res.json({
                 success: true,
                 payload: {
-                    user,
+                    user: userDto,
                     token
                 },
                 msg: 'User created successfully',
